3898752fb922f68a5b951c8de712cee89fe10d55
EliWood <ew20kt@snpsteam.com>
2023-05-29 14:15:14 -0400

    added import

--- src/main/java/com/example/drawing/Main.java
+++ src/main/java/com/example/drawing/Main.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
